f005e9af4b42a5901f98713c4c3f491bacd62e77
Your Name <sumisastri@gmail.com>
2019-09-25 14:50:54 +0100

    adds challenge 23

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const path = require('path');
 const films = require('./films-data');
 
 app.get('/films/data', (req, res) => res.send(films.filmsData));
+
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, '<path-to-build-folder>', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Kodiri-Kodflix app listening on port ${port}!`));
